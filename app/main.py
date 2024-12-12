e6ccca112b1ac464e4d7397f12e47f6d5717ce38
SoLuZ <112957977+Nimble05@users.noreply.github.com>
2024-12-12 14:01:07 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,15 +7,6 @@ from sqlalchemy.orm import sessionmaker, Session
 # Initialize FastAPI
 app = FastAPI()
 
-'''
-Database
-'''
-
-DATABASE_URL = "sqlite:///./todos.db"
-Base = declarative_base()
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 # Initialize
 Base.metadata.create_all(bind=engine)
 
