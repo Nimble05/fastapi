cef13a2372079037e78e1d9a4a310cd7c328c0fb
SoLuZ <112957977+Nimble05@users.noreply.github.com>
2024-12-12 13:59:45 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Column, Date, Interger, String, Boolean
+from .main import Base
 
+# Define Model
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
+    due_date = Column(Date, nullable=True)
